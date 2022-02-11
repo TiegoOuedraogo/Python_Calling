fca2546257e3a9fd0bcb6f349b3e174ce3546910
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2022-02-11 01:45:11 -0500

    single double and triple quotes works fine, for the imunability pring  the variable with id

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+single_quote = 'Hello Python I\' am using single quote'
+double_quote = "Hello  Python I \'am using \"double quote\" "
+print(single_quote)
+print(double_quote)
---
+++ StringConcatination.py
@@ -0,0 +1,9 @@
+message = 'The stock market for tech is:'
+price = "$1000"
+print(message + " " + price)
+# manipulating
+new_message = message + " " + price
+print(new_message)
+# string are imunable look what is going on here
+message = message + " " + price
+print(message) #this message is totality from different from the messgae on top
