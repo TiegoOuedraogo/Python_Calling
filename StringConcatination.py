f5a16a6203196e974eeeaaf0a6409d54d7192a5b
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2022-02-11 01:59:22 -0500

    concatination and string imunable understanding using variable id's

--- StringConcatination.py
+++ StringConcatination.py
@@ -1,4 +1,5 @@
 message = 'The stock market for tech is:'
+print(id(message)," This is for the first message")
 price = "$1000"
 print(message + " " + price)
 # manipulating
@@ -6,4 +7,4 @@ new_message = message + " " + price
 print(new_message)
 # string are imunable look what is going on here
 message = message + " " + price
-print(message) #this message is totality from different from the messgae on top
+print(id(message)," This is for the second message") #this message is totality from different from the messgae on top
